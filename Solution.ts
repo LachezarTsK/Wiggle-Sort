dbc49d260e0cce461fe22598385b803b718f1d1a
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-01 12:47:03 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,11 @@
+
+function wiggleSort(input: number[]): void {
+    for (let i = 1; i < input.length; i += 2) {
+        if (input[i - 1] > input[i]) {
+            [input[i - 1], input[i]] = [input[i], input[i - 1]];
+        }
+        if (i + 1 < input.length && input[i + 1] > input[i]) {
+            [input[i + 1], input[i]] = [input[i], input[i + 1]];
+        }
+    }
+};
